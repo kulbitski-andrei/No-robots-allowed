e2cf0287fb08f65372c21798e9a68a2d1e28bf5a
Andrei Kulbitski <andrei.kulbitski@gmail.com>
2024-08-10 10:27:18 +0200

    added fixture for sign up and log in tests

--- conftest.py
+++ conftest.py
@@ -11,6 +11,25 @@ from pages.add_contact_page import AddContactPage
 import test_data.constants as const
 from logger.log_setup import logger
 
+@pytest.fixture
+def browser_sign_up_log_in():
+    """
+    Fixture to set up and tear down the browser instance for testing contacts.
+    At the start of each test log in is performed, new John Doe contact is created.
+    At the start of each test created contact is deleted.
+    """
+    logger.info("Fixture: Setting up started")
+    chrome_options = Options()
+    chrome_options.add_argument("--disable-search-engine-choice-screen")
+    chrome_browser = webdriver.Chrome(options=chrome_options)
+    chrome_browser.implicitly_wait(5)
+    chrome_browser.get(const.URL)
+    yield chrome_browser
+    chrome_browser.get(const.URL_CONTACTS)
+    page_object = ContactListPage(chrome_browser)
+    page_object.logout()
+    chrome_browser.quit()
+
 
 @pytest.fixture
 def browser_contacts():
