e237b2906cae629ecdbafa7e833ef1ca1b1bea88
ETsiunel <y.tsyunel@gmail.com>
2024-08-25 18:26:02 +0300

    setup_pipelines_return_tests

---
+++ tests/api/users/conftest.py
@@ -0,0 +1,33 @@
+"""Fixtures API"""
+
+
+import pytest
+import requests
+from tests.api.data_api_users import BASE_URL, HEADERS, UserData
+
+
+@pytest.fixture
+def auth_token_and_user_data():
+    """Fixture to register a user and obtain an auth token."""
+    user_data = UserData.generate_user_data()
+
+    response = requests.post(f"{BASE_URL}/users",
+                             json=user_data, headers=HEADERS)
+    assert response.status_code == 201, (f"Expected status 201, "
+                                         f"but got {response.status_code}")
+
+    login_data = {
+        "email": user_data["email"],
+        "password": user_data["password"]
+    }
+    login_response = requests.post(f"{BASE_URL}/users/login",
+                                   json=login_data, headers=HEADERS)
+    assert login_response.status_code == 200, \
+        f"Expected status 200, but got {login_response.status_code}"
+
+    response_data = login_response.json()
+    # return {
+    #     "token": response_data["token"],
+    #     "user_data": user_data
+    # }
+    return response_data["token"], user_data
---
+++ tests/api/users/test_logout_user.py
@@ -0,0 +1,71 @@
+"""API tests Logout user"""
+
+
+import pytest
+import requests
+from tests.api.data_api_users import BASE_URL
+
+
+@pytest.mark.high
+@pytest.mark.smoke
+@pytest.mark.API
+def test_logout_user(auth_token_and_user_data):
+    """Positive tests: Successfully log out a user."""
+    token, _ = auth_token_and_user_data  # распаковываем кортеж
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    response = requests.post(f"{BASE_URL}/users/logout",
+                             headers=headers)
+
+    assert response.status_code == 200, (f"Expected status 200, "
+                                         f"but got {response.status_code}")
+    assert response.text == "", ("Expected empty response body "
+                                 "for successful logout")
+
+
+@pytest.mark.medium
+@pytest.mark.regress
+@pytest.mark.API
+def test_logout_user_invalid_token():
+    """Negative tests: Attempt to log out with an invalid token."""
+    invalid_token = "invalid_token"
+    headers = {
+        "Authorization": f"Bearer {invalid_token}"
+    }
+
+    response = requests.post(f"{BASE_URL}/users/logout", headers=headers)
+
+    assert response.status_code == 401, (f"Expected status 401, "
+                                         f"but got {response.status_code}")
+    response_data = response.json()
+    assert response_data.get("error") == "Please authenticate.", \
+        (f"Expected error message 'Please authenticate.', "
+         f"but got {response_data.get('error')}")
+
+
+@pytest.mark.medium
+@pytest.mark.regress
+@pytest.mark.API
+def test_logout_user_twice(auth_token_and_user_data):
+    """Negative tests: Attempt to log out twice."""
+    token, _ = auth_token_and_user_data
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    response1 = requests.post(f"{BASE_URL}/users/logout", headers=headers)
+    assert response1.status_code == 200, (f"Expected status 200, "
+                                          f"but got {response1.status_code}")
+    assert response1.text == "", ("Expected empty response body "
+                                  "for successful logout")
+
+    response2 = requests.post(f"{BASE_URL}/users/logout",
+                              headers=headers)
+    assert response2.status_code == 401, (f"Expected status 401, "
+                                          f"but got {response2.status_code}")
+    response_data = response2.json()
+    assert response_data.get("error") == "Please authenticate.", \
+        (f"Expected error message 'Please authenticate.', "
+         f"but got {response_data.get('error')}")
